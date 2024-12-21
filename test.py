3adb4d1753607ce859c4b74492c82bc7d55724c4
Ahmed Hassan Darwesh <ah.darwesh@nu.edu.eg>
2024-12-21 23:15:20 +0200

    Merged changes from main

---
+++ test.py
@@ -0,0 +1,43 @@
+from dasapp.models import CustomUser  # استيراد النموذج CustomUser بدلاً من User
+from django.test import TestCase
+from django.urls import reverse
+
+class AdminViewsTest(TestCase):
+
+    def setUp(self):
+        self.admin_user = CustomUser.objects.create_superuser(
+            username='admin',
+            password='admin123',
+            email='admin@example.com'
+        )
+        self.client.login(username='admin', password='admin123') 
+
+    def test_admin_home_view(self):
+        response = self.client.get(reverse('admin_home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_specialization_view(self):
+        response = self.client.get(reverse('add_specilizations'))
+        self.assertEqual(response.status_code, 200)
+
+
+class AuthViewsTest(TestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username='testuser',
+            password='testpassword',
+            email='testuser@example.com'
+        )
+
+    def test_login_view(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Login')
+
+    def test_do_login(self):
+        response = self.client.post(reverse('doLogin'), {
+            'email': 'testuser@example.com',
+            'password': 'testpassword',
+        })
+        self.assertRedirects(response, reverse('admin_home'))         
